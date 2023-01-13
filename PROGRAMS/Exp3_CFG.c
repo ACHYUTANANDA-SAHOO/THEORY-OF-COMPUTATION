d171a21af49e795f5d604f3ee67821bb323f2748
ACHYUTANANDA-SAHOO <113246116+ACHYUTANANDA-SAHOO@users.noreply.github.com>
2023-01-13 21:50:49 +0530

    Add files via upload

---
+++ PROGRAMS/Exp3_CFG.c
@@ -0,0 +1,45 @@
+// To write a C program to check whether a string belongs to the grammar
+// S → 0 A 1
+// A → 0 A | 1 A | ε
+
+#include<stdio.h>
+#include<string.h>
+
+int checkValidity(char ip[], int l){
+
+    int iv = 0;
+
+    for(int i = 0;i<l;i++){
+        if(ip[i] == '0' || ip[i] == '1')
+            continue;
+        else{
+            iv = 1;
+            break;
+        }
+    }
+
+    return iv;
+}
+
+int main(){
+    char input[100];
+    while(1){
+        printf("Enter a String : ");
+        scanf("%s",&input);
+
+        int l = strlen(input);
+
+        int invalid = checkValidity(input,l);
+
+        if(invalid != 1){
+            if(input[0] == '0' && input[l-1] == '1')
+                printf("String Accepted ! \n\n ");
+            else
+                printf("String Rejected ! \n\n");
+        }
+        else
+            printf("Enter a Valid String over E = {0,1} .\n\n");
+    }
+
+    return 0;
+}
